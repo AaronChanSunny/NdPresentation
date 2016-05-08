8d9bdc076c1a3206d36a0b8baa5dbbff396ba1eb
AaronChanSunny <aaronchansunny@gmail.com>
2016-05-08 12:00:20 +0800

    Add Item layout, empty Item adapter.

---
+++ app/src/main/java/me/aaronchan/ndpresentation/Item.java
@@ -0,0 +1,52 @@
+package me.aaronchan.ndpresentation;
+
+public class Item {
+    private boolean mIsHeader;
+    private String mTitle;
+    private String mDesc;
+    private Class mActivity;
+
+    public Item(boolean isHeader, String title) {
+        mIsHeader = isHeader;
+        mTitle = title;
+    }
+
+    public Item(boolean isHeader, String title, String desc, Class activity) {
+        mIsHeader = isHeader;
+        mTitle = title;
+        mDesc = desc;
+        mActivity = activity;
+    }
+
+    public boolean isHeader() {
+        return mIsHeader;
+    }
+
+    public void setIsHeader(boolean isHeader) {
+        mIsHeader = isHeader;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public String getDesc() {
+        return mDesc;
+    }
+
+    public void setDesc(String desc) {
+        mDesc = desc;
+    }
+
+    public Class getActivity() {
+        return mActivity;
+    }
+
+    public void setActivity(Class activity) {
+        mActivity = activity;
+    }
+}
---
+++ app/src/main/java/me/aaronchan/ndpresentation/ItemAdapter.java
@@ -0,0 +1,7 @@
+package me.aaronchan.ndpresentation;
+
+/**
+ * Created by aaronchan on 16/5/8.
+ */
+public class ItemAdapter {
+}
