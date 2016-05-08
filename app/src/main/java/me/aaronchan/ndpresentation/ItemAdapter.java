62043c06ec96fafc459d8108164b7380ab713141
AaronChanSunny <aaronchansunny@gmail.com>
2016-05-08 14:22:05 +0800

    Complete MainActivity list.

--- app/src/main/java/me/aaronchan/ndpresentation/ItemAdapter.java
+++ app/src/main/java/me/aaronchan/ndpresentation/ItemAdapter.java
@@ -1,7 +1,104 @@
 package me.aaronchan.ndpresentation;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by aaronchan on 16/5/8.
  */
-public class ItemAdapter {
+public class ItemAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_ITEM = 1;
+
+    private List<Item> mItems;
+
+    public ItemAdapter(List<Item> items) {
+        mItems = items;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder holder;
+
+        if (viewType == TYPE_ITEM) {
+            View itemView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_content, parent, false);
+            holder = new ItemViewHolder(itemView);
+        } else {
+            View headerView = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_header, parent, false);
+            holder = new HeaderViewHolder(headerView);
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        Item item = mItems.get(position);
+
+        if (holder.getItemViewType() == TYPE_ITEM) {
+            ItemViewHolder itemHolder = (ItemViewHolder) holder;
+
+            itemHolder.mTitle.setText(item.getTitle());
+            itemHolder.mDesc.setText(item.getDesc());
+        } else {
+            HeaderViewHolder headerHolder = (HeaderViewHolder) holder;
+
+            headerHolder.mHeader.setText(item.getTitle());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mItems.get(position).isHeader() ? TYPE_HEADER : TYPE_ITEM;
+    }
+
+    class HeaderViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mHeader;
+
+        public HeaderViewHolder(View itemView) {
+            super(itemView);
+
+            mHeader = (TextView) itemView.findViewById(R.id.tv_header);
+        }
+    }
+
+    class ItemViewHolder extends RecyclerView.ViewHolder {
+
+        TextView mTitle;
+        TextView mDesc;
+
+        public ItemViewHolder(final View itemView) {
+            super(itemView);
+
+            mTitle = (TextView) itemView.findViewById(R.id.tv_title);
+            mDesc = (TextView) itemView.findViewById(R.id.tv_desc);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Item item = mItems.get(getLayoutPosition());
+                    Context context = itemView.getContext();
+
+                    Intent intent = new Intent(context, item.getActivity());
+                    context.startActivity(intent);
+                }
+            });
+        }
+    }
+
 }
--- app/src/main/java/me/aaronchan/ndpresentation/MainActivity.java
+++ app/src/main/java/me/aaronchan/ndpresentation/MainActivity.java
@@ -4,12 +4,21 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<Item> mItems;
+    private RecyclerView mRvItem;
+    private ItemAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +26,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        initData();
+
+        initView();
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+
     }
 
     @Override
@@ -48,5 +63,21 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void initView() {
+        mRvItem = (RecyclerView) findViewById(R.id.rv_item);
+        mRvItem.setLayoutManager(new LinearLayoutManager(this));
+        mRvItem.setAdapter(mAdapter);
+    }
+
+    private void initData() {
+        mItems = new ArrayList<>();
+
+        mItems.add(new Item(true, "Android Async Framework"));
+        mItems.add(new Item(false, "Handler Without Looper", "Create handler in thread without " +
+                "looper throw exception.", HandlerCreationActivity.class));
+
+        mAdapter = new ItemAdapter(mItems);
     }
 }
