418474717f799a088c9b17ef48e243de69896730
AaronChanSunny <aaronchansunny@gmail.com>
2016-05-09 16:36:57 +0800

    Add IntentService worker thread tag.

--- app/src/main/java/me/aaronchan/ndpresentation/SimpleService.java
+++ app/src/main/java/me/aaronchan/ndpresentation/SimpleService.java
@@ -3,7 +3,6 @@ package me.aaronchan.ndpresentation;
 import android.app.IntentService;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Log;
 
@@ -16,10 +15,9 @@ public class SimpleService extends IntentService {
     private static final String KEY_TAG = SimpleService.class.getName() + ".KEY_TAG";
 
     public static void actionStart(Context context, String tag) {
-        Bundle args = new Bundle();
-        args.putString(KEY_TAG, tag);
-
         Intent intent = new Intent(context, SimpleService.class);
+        intent.putExtra(KEY_TAG, tag);
+
         context.startService(intent);
     }
 
@@ -31,7 +29,7 @@ public class SimpleService extends IntentService {
     protected void onHandleIntent(Intent intent) {
         SystemClock.sleep(2000);
 
-        Log.d(TAG, "Task in " + Thread.currentThread().getName() + " done.");
+        Log.d(TAG, "Task in " + Thread.currentThread().getName() + " " + intent.getStringExtra(KEY_TAG) + " done.");
     }
 
     @Override
